3ce41810eccc2fe52f794f4b104d8b10b83cea29
Игорь Потапов <igpotapov@gmail.com>
2023-08-30 14:23:17 +0300

    Трудоемкость 5-Snapshot

---
+++ Class/WorkTimeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Dispetcher2.Class
+{
+    public class WorkTimeViewModel
+    {
+        TimeSpan total;
+        TimeSpan past;
+        
+        public WorkTimeViewModel(TimeSpan total, TimeSpan past)
+        {
+            this.total = total;
+            this.past = past;
+        }
+        public long TotalDays
+        {
+            get
+            {
+                var y = total.TotalDays;
+                return Convert.ToInt64(y);
+            }
+        }
+
+        public long PastDays
+        {
+            get
+            {
+                var y = past.TotalDays;
+                return Convert.ToInt64(y);
+            }
+        }
+
+        public long RestDays
+        {
+            get
+            {
+                TimeSpan r = total.Subtract(past);
+                var y = r.TotalDays;
+                return Convert.ToInt64(y);
+            }
+        }
+    }
+}
