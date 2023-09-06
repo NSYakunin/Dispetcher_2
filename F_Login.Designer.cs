96de17188fd7e317f95dae78f91b86fe6ba34965
Nickolay <druumm36@gmail.com>
2023-09-06 09:51:22 +0300

    Визуальные правки

--- F_Login.Designer.cs
+++ F_Login.Designer.cs
@@ -42,10 +42,10 @@ namespace Dispetcher2
             this.lbl_Welcome = new System.Windows.Forms.Label();
             this.gB_NewLogin = new System.Windows.Forms.GroupBox();
             this.tB_NewLogin = new System.Windows.Forms.TextBox();
+            this.mychB_NewLogin = new Dispetcher2.Controls.MyCheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.mychB_NewLogin = new Dispetcher2.Controls.MyCheckBox();
             this.tLP_Autorization.SuspendLayout();
             this.gB_NewLogin.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -80,7 +80,7 @@ namespace Dispetcher2
             this.tLP_Autorization.Controls.Add(this.gB_NewLogin, 2, 6);
             this.tLP_Autorization.Controls.Add(this.lbl_User, 1, 13);
             this.tLP_Autorization.Controls.Add(this.mychB_NewLogin, 2, 13);
-            this.tLP_Autorization.Controls.Add(this.panel1, 3, 11);
+            this.tLP_Autorization.Controls.Add(this.panel1, 2, 11);
             this.tLP_Autorization.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tLP_Autorization.Location = new System.Drawing.Point(0, 0);
             this.tLP_Autorization.Name = "tLP_Autorization";
@@ -205,37 +205,6 @@ namespace Dispetcher2
             this.tB_NewLogin.TabIndex = 1;
             this.tB_NewLogin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tB_NewLogin_KeyDown);
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(948, 369);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(135, 78);
-            this.panel1.TabIndex = 1007;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "main",
-            "test"});
-            this.comboBox1.Location = new System.Drawing.Point(3, 31);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(127, 21);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Выберите подключение:";
-            // 
             // mychB_NewLogin
             // 
             this.mychB_NewLogin.Appearance = System.Windows.Forms.Appearance.Button;
@@ -251,6 +220,39 @@ namespace Dispetcher2
             this.mychB_NewLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.mychB_NewLogin.UseVisualStyleBackColor = true;
             this.mychB_NewLogin.CheckedChanged += new System.EventHandler(this.mychB_NewLogin_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(446, 369);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 78);
+            this.panel1.TabIndex = 1007;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "main",
+            "test"});
+            this.comboBox1.Location = new System.Drawing.Point(39, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 32);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите сервер:";
             // 
             // F_Login
             // 
