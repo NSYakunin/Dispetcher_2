f9b2ef46a0b462f45fe92849c44f6ea5d562cd57
Nickolay <druumm36@gmail.com>
2024-11-13 15:59:04 +0300

    Поменял в свойствах файла app.config с "Не копировать" на "Копировать более поздюю версию"

---
+++ Class/TKControlData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispetcher2.Class
+{
+    public class OTKControlData
+    {
+        public CheckBoxState[] States { get; set; }
+        public string LastUser { get; set; }
+        public DateTime LastChangeDate { get; set; }
+
+        public OTKControlData()
+        {
+            States = new CheckBoxState[] { CheckBoxState.Unchecked, CheckBoxState.Unchecked, CheckBoxState.Unchecked };
+            LastUser = string.Empty;
+            LastChangeDate = DateTime.MinValue;
+        }
+    }
+}
